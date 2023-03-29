0af3e6b415db6daa2811c3947d2987e7bd40b9f1
Gustavo Costa <gustavocosta.cba@gmail.com>
2023-03-29 12:12:12 -0400

    feat: dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,29 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import * as crypto from 'node:crypto'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = knex('sqlite_schema').select('*')
-  return tables
+  const transaction = await knex('transactions').select('*')
+  return transaction
+})
+
+app.post('/insert', async () => {
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'teste',
+      amount: 2000,
+    })
+    .returning('*')
+  return transaction
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('Server on')
